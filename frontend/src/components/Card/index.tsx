6a4be427fdb71973be3627db8279e15fa7363777
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-10-11 09:45:44 -0300

    :ok_hand:style organizing the card
    :ok_hand:style organizing the card

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,15 +1,14 @@
 import { Caixa, CaixaTexto } from "./styles";
-
-function index(){
+interface Product { image: string, name: string, price: string } 
+function index( { image, name, price }: Product ) {
   return(
-   
         <Caixa>
-        <img src="/assets/n42ogaQn32o.png" />
+        <img src={image} />
           <CaixaTexto>
-            <p className="descricao">Caneca de cerâmica rústica</p>
+            <p className="descricao">{name}</p>
             <svg xmlns="http://www.w3.org/2000/svg" width="228" height="1" viewBox="0 0 228 1" fill="none">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M228 0.99998L8.74228e-08 1L0 0L228 -1.99324e-05L228 0.99998Z" fill="#DCE2E6"/></svg>
-            <p className="preco">R$ 40,00</p>
+            <p className="preco">{price}</p>
           </CaixaTexto>
         </Caixa>
     
